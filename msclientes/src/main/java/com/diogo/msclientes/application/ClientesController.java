06b05cdce0a64f2120b8df6dd07ffcc4584ef241
Diog007 <diogo.mz123@gmail.com>
2024-03-01 11:01:36 -0300

    Created customer controller

---
+++ msclientes/src/main/java/com/diogo/msclientes/application/ClientesController.java
@@ -0,0 +1,11 @@
+package com.diogo.msclientes.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClientesController {
+
+}
