d274cf1c8871216ccc221ff145383d240068edd6
Diog007 <diogo.mz123@gmail.com>
2024-03-01 11:00:16 -0300

    Created customer entity

---
+++ msclientes/src/main/java/com/diogo/msclientes/domain/Cliente.java
@@ -0,0 +1,27 @@
+package com.diogo.msclientes.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String cpf;
+    @Column
+    private String nome;
+    @Column
+    private String idade;
+
+    public Cliente(String cpf, String nome, String idade) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
