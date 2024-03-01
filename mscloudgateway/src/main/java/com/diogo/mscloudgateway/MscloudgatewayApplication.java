03e0b37236963df09779b876f7f18c58840d40da
Diog007 <diogo.mz123@gmail.com>
2024-03-01 12:52:11 -0300

    Created msCloudGateway

---
+++ mscloudgateway/src/main/java/com/diogo/mscloudgateway/MscloudgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.diogo.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+
+}
