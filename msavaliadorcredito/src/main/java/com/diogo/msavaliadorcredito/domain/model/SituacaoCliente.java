6a343f3e096907b43c19beaea3a3a0b7dea7bf19
Diog007 <diogo.mz123@gmail.com>
2024-03-06 14:31:26 -0300

    Created domain classes

---
+++ msavaliadorcredito/src/main/java/com/diogo/msavaliadorcredito/domain/model/SituacaoCliente.java
@@ -0,0 +1,12 @@
+package com.diogo.msavaliadorcredito.domain.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SituacaoCliente {
+    private DadosCliente cliente;
+    private List<CartaoCliente> cartoes;
+
+}
