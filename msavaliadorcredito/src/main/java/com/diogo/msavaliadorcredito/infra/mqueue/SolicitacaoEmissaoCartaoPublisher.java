299d36d650cb978a396e4da015f401c90696079f
Diog007 <diogo.mz123@gmail.com>
2024-03-08 13:41:08 -0300

    Implemented the queue for card issuance RabbitMQ

---
+++ msavaliadorcredito/src/main/java/com/diogo/msavaliadorcredito/infra/mqueue/SolicitacaoEmissaoCartaoPublisher.java
@@ -0,0 +1,29 @@
+package com.diogo.msavaliadorcredito.infra.mqueue;
+
+import com.diogo.msavaliadorcredito.domain.model.DadosSolicitacaoEmissaoCartao;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SolicitacaoEmissaoCartaoPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final Queue queueEmissaoCartoes;
+
+    public void solicitarCartao(DadosSolicitacaoEmissaoCartao dados) throws JsonProcessingException {
+        var json = converteIntoJson(dados);
+        rabbitTemplate.convertAndSend(queueEmissaoCartoes.getName(), json);
+    }
+
+    private String converteIntoJson(DadosSolicitacaoEmissaoCartao dados)throws JsonProcessingException{
+        ObjectMapper mapper = new ObjectMapper();
+        var json = mapper.writeValueAsString(dados);
+        return json;
+    }
+
+}
